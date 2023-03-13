c7bbd7a26f332c4262d332718d0a6ede24f02479
Jun-yong-lee <wnsdyd1569@naver.com>
2023-03-13 15:51:27 +0900

    Add time

--- make_SVM_dataset.py
+++ make_SVM_dataset.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 webcam1 = cv2.VideoCapture(0)
 # webcam2 = cv2.VideoCapture(4)
@@ -12,6 +13,13 @@ if not webcam2.isOpened():
     print("Could not open webcam2")
     exit()
 
+count = 0
+
+status1, frame1 = webcam1.read()
+status2, frame2 = webcam2.read()
+
+time.sleep(2)
+
 while webcam1.isOpened():
     status1, frame1 = webcam1.read()
     status2, frame2 = webcam2.read()
@@ -23,12 +31,12 @@ while webcam1.isOpened():
     count = format(count, '08')
     
     cv2.imshow("frame1", frame1)
-    cv2.imwrite("dataset/depth/" + str(count) + ".png", frame1)
+    cv2.imwrite("dataset_SVM/camera1/" + str(count) + ".png", frame1)
     
     cv2.imshow("frame2", frame2)
-    cv2.imwrite("dataset/rgb/" + str(count) + ".png", frame2)
+    cv2.imwrite("dataset_SVM/camera2/" + str(count) + ".png", frame2)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(100) & 0xFF == ord('q'):
         break
 
 webcam1.release()
